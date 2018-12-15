589b9edf01431d09bbd23ed71404dede821f8380
Dafei Ning <ningdafei@gmail.com>
2018-12-15 00:32:17 -0800

    trans to generic methods of adding ingredients

--- src/components/Dish/Dish.js
+++ src/components/Dish/Dish.js
@@ -4,13 +4,17 @@ import './Dish.css';
 import DishIngredient from './DishIngredient/DishIngredient';
 
 const dish = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <DishIngredient key={igKey + i} type={igKey} />;
+            });
+        });
     return (
         <div className="Dish">
             <DishIngredient type="bread-top" />
-            <DishIngredient type="bacon" />
-            <DishIngredient type="salad" />
-            <DishIngredient type="cheese" />
-            <DishIngredient type="meat" />
+            {transformedIngredients}
             <DishIngredient type="bread-bottom" />
 
         </div>
