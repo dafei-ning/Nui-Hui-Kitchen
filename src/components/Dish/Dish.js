5dbcfce94a90f85daee266ab705c1df72dd98bb4
Dafei Ning <ningdafei@gmail.com>
2018-12-15 16:43:35 -0800

    flattern the array to sum the ingredients

--- src/components/Dish/Dish.js
+++ src/components/Dish/Dish.js
@@ -5,12 +5,19 @@ import DishIngredient from './DishIngredient/DishIngredient';
 
 const dish = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <DishIngredient key={igKey + i} type={igKey} />;
             });
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className="Dish">
             <DishIngredient type="bread-top" />
