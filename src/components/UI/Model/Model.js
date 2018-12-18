5a33958ca7e575b2ac851bcd7e57e764d63fa332
Dafei Ning <ningdafei@gmail.com>
2018-12-17 17:50:05 -0800

    add backdrop

--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './Model.css';
 
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
+
 const model = (props) => (
-    <div
-        className="Model"
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-        {props.children}
-    </div>
+    <Aux>
+        <Backdrop show={props.show} />
+        <div
+            className="Model"
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            {props.children}
+        </div>
+    </Aux>
 );
 
 export default model;
