fdcfe0d6cc7109298376824a3334ad66c1c306ba
Dafei Ning <ningdafei@gmail.com>
2018-12-17 23:44:43 -0800

    add scratch toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './Layout.css';
 
 import Aux from '../../hoc/Aux';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 
 const layout = (props) => (
     <Aux>
+        <Toolbar />
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main className="Content">
             {props.children}
