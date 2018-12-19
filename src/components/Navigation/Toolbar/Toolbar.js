0efcb44a94d43193742dbaa1db1b9dc8a46f95bd
Dafei Ning <ningdafei@gmail.com>
2018-12-19 01:53:21 -0800

    optimize toggle button

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,10 +7,10 @@ import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
     <header className="Toolbar">
-        <DrawerToggle  clicked={props.Toggle}/>
+        <DrawerToggle clicked={props.Toggle} />
 
         <div className="LogoInToolbar">
-            <Logo  />
+            <Logo />
         </div>
         <nav className="DesktopOnly">
             <NavigationItems />
