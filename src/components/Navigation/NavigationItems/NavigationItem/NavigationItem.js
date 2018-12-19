45843498706449d511916d37b35079a0d93f0b6c
Dafei Ning <ningdafei@gmail.com>
2018-12-18 23:56:38 -0800

    adjust logo size on responsive

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './NavigationItem.css';
 
-const navigationItem = (props) => (
-    <li className="NavigationItem">
-        <a
-            href={props.link}
-            className={props.active ? "a.active" : null}
-        >
-            {props.children}
-        </a>
-    </li>
-);
+const navigationItem = (props) => {
+    let classes = [];
+    classes.push('active');
+    classes.push('hover');
+
+    return (
+        <li className="NavigationItem">
+            <a
+                href={props.link}
+                className={props.active ? classes.join(' ') : null}
+            >
+                {props.children}
+            </a>
+        </li>
+    );
+};
 
 
 export default navigationItem;
