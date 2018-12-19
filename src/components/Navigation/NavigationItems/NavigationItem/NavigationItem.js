26bb8b6435f8624938b1bc5ef8afd807fd34602a
Dafei Ning <ningdafei@gmail.com>
2018-12-18 22:32:31 -0800

    add navigation structure

---
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './NavigationItem.css';
+
+const navigationItem = (props) => (
+    
+);
+
+
+export default navigationItem;
---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NavigationItems.css';
+
+const navigationItems = (props) => (
+    <ul>
+        <li><a href="/">a link</a></li>
+    </ul>
+);
+
+
+export default navigationItems;
